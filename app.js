a32c3c75bfa0180252002c62d2a36a4cd3a1b7fd
Haashir-Shakeel <david10james99@gmail.com>
2023-01-19 16:51:42 +0500

    getting location from command Line

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const geoCode = require('./utils/geoCode')
 const forecast = require('./utils/forecast')
 
-    geoCode('Boston', (error,data)=>{
-        console.log('Error: ', error);
-        console.log('Data: ', data);
+    const address = process.argv[2]
+
+    if (!address){
+        console.log('please provide address');
+    }
+    
+    geoCode(address, (error,data)=>{
+        if (error !== 'None'){
+            return console.log(error);
+        }
+
+        forecast(data.latitude, data.longitude, (error,forecastData)=>{
+
+            if (error!=='None'){
+                return console.log(error);
+            }
+            console.log('Location: ' + data.location);
+            console.log('Weather: ', forecastData);
+        })
+
     })
 
 
-    forecast(-71.0598, 42.3584, (error,data)=>{
-        console.log('Error: ', error);
-        console.log('Data: ', data);
-    })
+    
